5cc07c2c08dab65f2582565ef9000c52f98b3d50
Niklas Lundberg <info@nlundberg.se>
2018-10-05 14:17:43 +0200

    Added custom sink logging.

---
+++ src/Arbor.Tooler.Tests.Integration/MySink.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Arbor.Tooler.Tests.Integration
+{
+    public class MySink : ILogEventSink
+    {
+        private readonly IFormatProvider _formatProvider;
+        private readonly Action<string> _logAction;
+
+        public MySink(Action<string> logAction, IFormatProvider formatProvider)
+        {
+            _logAction = logAction;
+            _formatProvider = formatProvider;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            var message = logEvent.RenderMessage(_formatProvider);
+            _logAction(DateTimeOffset.Now + " " + message);
+        }
+    }
+}
---
+++ src/Arbor.Tooler.Tests.Integration/MySinkExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Serilog;
+using Serilog.Configuration;
+
+namespace Arbor.Tooler.Tests.Integration
+{
+    public static class MySinkExtensions
+    {
+        public static LoggerConfiguration MySink(
+            this LoggerSinkConfiguration loggerConfiguration,
+            Action<string> logAction,
+            IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new MySink(logAction, formatProvider));
+        }
+    }
+}
